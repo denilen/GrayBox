bdf8eae3ff6c2cd1bbaf9d970498024fe2c40397
Denis Yakushev <dennlen@gmail.com>
2021-12-07 15:37:08 +0500

    .NET 6.0

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var number = 5;
+         
+            Console.WriteLine($"Число до метода Increment: {number}");
+            Increment(ref number);
+            Console.WriteLine($"Число после метода Increment: {number}");
+
+            static void Increment(ref int n)
+            {
+                n++;
+                Console.WriteLine($"Число в методе Increment: {n}");
+                RefIncrement(ref n);
+            }
+
+            static void RefIncrement(ref int n)
+            {
+                n++;
+                Console.WriteLine($"Число в методе RefIncrement: {n}");
+            }
+        }
+    }
+}
