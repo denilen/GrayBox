202a0ac0fbf932df3d14dde824f3b5870d1bbde5
Denis Yakushev <dennilen@gmail.com>
2024-03-15 16:05:07 +0500

    work in progress

---
+++ gb.grpc.client/Program.cs
@@ -0,0 +1,29 @@
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using GrpcGreeter;
+
+namespace gb.grpc.client
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+        static async Task Main(string[] args)
+        {
+            // создаем канал для обмена сообщениями с сервером
+            // параметр - адрес сервера gRPC
+            using var channel = GrpcChannel.ForAddress("https://localhost:5002");
+            // создаем клиента
+            var client = new Greeter.GreeterClient(channel);
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            // обмениваемся сообщениями с сервером
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+            Console.WriteLine("Ответ сервера: " + reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
